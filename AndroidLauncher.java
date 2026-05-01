ce11f5fcd16175d346fafaef058a84a8eeab077b
sumankrchakravarty-alt <suman.kr.chakravarty@gmail.com>
2026-05-01 08:40:04 +0530

    Add AndroidLauncher.java file

---
+++ AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.tankgame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useWakelock = true;
+        config.useCompass = false;
+        config.useAccelerometer = false;
+        initialize(new TankShootingGame(), config);
+    }
+}
